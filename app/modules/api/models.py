ebbdbcc43a650c2c1a442dac70b995612f74ba44
Ashley Collinge <ashley.collinge@outlook.comp>
2019-05-04 23:52:42 +0100

    added a load more models

--- app/modules/api/models.py
+++ app/modules/api/models.py
@@ -1,9 +1,150 @@
 from app import db
 
-class Tickets(db.Model):
+class Incident(db.Model):
+	"""
+
+	"""
+	id = db.Column(db.Integer, primary_key=True)
+	date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
+	date_modified = db.Column(db.DateTime, default=db.func.current_timestamp(),
+							  onupdate=db.func.current_timestamp())
+	subject = db.Column(db.String())
+	description = db.Column(db.String())
+	priority = db.relationship('IncidentPriority', backref='incident', lazy=True)
+	status = db.relationship('IncidentStatus', backref='incident', lazy=True)
+	agent_group = db.relationship('AgentGroup', backref='incident', lazy=True)
+	agent_assigned = db.relationship('Agent', backref='incident', lazy=True)
+	department = db.relationship('RequesterDepartment', backref='incident', lazy=True)
+	category = db.relationship('IncidentCategory', backref='incident', lazy=True)
+	sub_category = db.relationship('IncidentSubCategory', backref='incident', lazy=True)
+	requester = db.relationship('Requester', backref='incident', lazy=True)
+	notes = db.relationship('Note', backref='incident', lazy=True)
+
+class IncidentStatus(db.Model):
 	"""
 	"""
 	id = db.Column(db.Integer, primary_key=True)
 	date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
 	date_modified = db.Column(db.DateTime, default=db.func.current_timestamp(),
 							  onupdate=db.func.current_timestamp())
+	name = db.Column(db.String())
+	incidents = db.Column(db.Integer, db.ForeignKey('incident.id'), nullable=False)
+
+
+class IncidentPriority(db.Model):
+	"""
+	"""
+	id = db.Column(db.Integer, primary_key=True)
+	date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
+	date_modified = db.Column(db.DateTime, default=db.func.current_timestamp(),
+							  onupdate=db.func.current_timestamp())
+	name = db.Column(db.String())
+	incidents = db.Column(db.Integer, db.ForeignKey('incident.id'), nullable=False)
+
+
+class IncidentNote(db.Model):
+	"""
+	Contains all notes (private/public) on tickets
+	"""
+	id = db.Column(db.Integer, primary_key=True)
+	date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
+	date_modified = db.Column(db.DateTime, default=db.func.current_timestamp(),
+							  onupdate=db.func.current_timestamp())
+	type = db.Column(db.String()) # private/public note
+	main_body = db.Column(db.String())
+	agent = db.Column(db.Integer, db.ForeignKey('agent.id'), nullable=False)
+	email = db.Column(db.Integer) # 1 for email, 0 for no email
+	incident_id = db.Column(db.Integer, db.ForeignKey('incident.id'), nullable=False)
+
+class IncidentCategory(db.Model):
+	"""
+	"""
+	id = db.Column(db.Integer, primary_key=True)
+	date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
+	date_modified = db.Column(db.DateTime, default=db.func.current_timestamp(),
+							  onupdate=db.func.current_timestamp())
+	name = db.Column(db.String())
+	incident_id = db.Column(db.Integer, db.ForeignKey('incident.id'), nullable=False)
+	sub_categories = db.relationship('IncidentSubCategory', backref='incidentcategory', lazy=True)
+
+
+class IncidentImpact(db.Model):
+	"""
+	"""
+	id = db.Column(db.Integer, primary_key=True)
+	date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
+	date_modified = db.Column(db.DateTime, default=db.func.current_timestamp(),
+							  onupdate=db.func.current_timestamp())
+	name = db.Column(db.String())
+	incident_id = db.Column(db.Integer, db.ForeignKey('incident.id'), nullable=False)
+
+
+class IncidentSubCategory(db.Model):
+	"""
+	"""
+	id = db.Column(db.Integer, primary_key=True)
+	date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
+	date_modified = db.Column(db.DateTime, default=db.func.current_timestamp(),
+							  onupdate=db.func.current_timestamp())
+	name = db.Column(db.String())
+	incident_id = db.Column(db.Integer, db.ForeignKey('incident.id'), nullable=False)
+	category = db.Column(db.Integer, db.ForeignKey('incidentcategory.id'), nullable=False)
+
+
+class IncidentUrgency(db.Model):
+	"""
+	"""
+	id = db.Column(db.Integer, primary_key=True)
+	date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
+	date_modified = db.Column(db.DateTime, default=db.func.current_timestamp(),
+							  onupdate=db.func.current_timestamp())
+	name = db.Column(db.String())
+
+
+class Requester(db.Model):
+	"""
+	"""
+	id = db.Column(db.Integer, primary_key=True)
+	date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
+	date_modified = db.Column(db.DateTime, default=db.func.current_timestamp(),
+							  onupdate=db.func.current_timestamp())
+	department = db.relationship('IncidentDepartment', backref='requester', lazy=True)
+	incidents = db.Column(db.Integer, db.ForeignKey('incident.id'), nullable=False)
+
+
+class Agent(db.Model):
+	"""
+	"""
+	id = db.Column(db.Integer, primary_key=True)
+	date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
+	date_modified = db.Column(db.DateTime, default=db.func.current_timestamp(),
+							  onupdate=db.func.current_timestamp())
+	# group = # link back to AgentGroups
+	name = db.Column(db.String())
+	email_address = db.Column(db.String())
+	notes = db.relationship('IncidentNote', backref='agent', lazy=True)
+	incidents = db.Column(db.Integer, db.ForeignKey('incident.id'), nullable=False)
+
+
+class RequesterDepartment(db.Model):
+	"""
+	"""
+	id = db.Column(db.Integer, primary_key=True)
+	date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
+	date_modified = db.Column(db.DateTime, default=db.func.current_timestamp(),
+							  onupdate=db.func.current_timestamp())
+	name = db.Column(db.String())
+	requesters = db.Column(db.Integer, db.ForeignKey('requester.id'), nullable=False)
+	incidents = db.Column(db.Integer, db.ForeignKey('incident.id'), nullable=False)
+
+
+class AgentGroups(db.Model):
+	"""
+	"""
+	id = db.Column(db.Integer, primary_key=True)
+	date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
+	date_modified = db.Column(db.DateTime, default=db.func.current_timestamp(),
+							  onupdate=db.func.current_timestamp())
+	agents = db.Column(db.Integer, db.ForeignKey('agent.id'), nullable=False)
+	name = db.Column(db.String())
+	incidents = db.Column(db.Integer, db.ForeignKey('incident.id'), nullable=False)
--- app/modules/api/views.py
+++ app/modules/api/views.py
@@ -1,47 +1,47 @@
 from flask import Flask
 from flask import Blueprint, request, jsonify
 from app import db
-from app.modules.api.models import Tickets
+from app.modules.api.models import Incident
 
 # Define the blueprint: 'auth', set its url prefix: app.url/auth
 mod_api = Blueprint('api', __name__, url_prefix='/api')
 
 
-@mod_api.route('/tickets/<int:ticket_id>', methods=['GET'])
-def get_tickets(ticket_id: str = "") -> jsonify:
+@mod_api.route('/incident/<int:incident_id>', methods=['GET'])
+def get_incidents(incident_id: str = "") -> jsonify:
 	"""
-	Retrieves a list of all tickets and returns the basic information
+	Retrieves a list of all incidents and returns the basic information
 	for each ticket. Further API calls would be required to get further
 	information.
 
 	Args
-		:ticket_id (str): Unique ID for the ticket.
+		:incident_id (str): Unique ID for the ticket.
 
 	Returns:
 		Returns json message.
 	"""
-	tickets = []
-	ticket_results = db.session.query(Tickets).all()
-	for ticket in ticket_results:
-		new_ticket = {}
-		new_ticket['id'] = ticket.id
-		tickets.append(new_ticket)
-	return jsonify({"Data":tickets})
+	incidents = []
+	incident_results = db.session.query(Incident).all()
+	for incident in incident_results:
+		new_incident = {}
+		new_incident['id'] = incident.id
+		incidents.append(new_incident)
+	return jsonify({"Data":incidents})
 
 
-@mod_api.route('/tickets', methods=['POST'])
-def new_ticket() -> jsonify:
+@mod_api.route('/incident/', methods=['POST'])
+def new_incident() -> jsonify:
 	"""
-	Adds a new ticket and returns the ticket_id
+	Adds a new incident and returns the incident_id
 	assigned.
 
 	Returns:
-		Returns ticket_id of the new ticket entry.
+		Returns incident_id of the new ticket entry.
 	"""
-	new_ticket = Tickets()
-	db.session.add(new_ticket)
+	new_incident = Incident()
+	db.session.add(new_incident)
 	db.session.commit()
-	return jsonify({"Ticket_ID":new_ticket.id})
+	return jsonify({"Ticket_ID":new_incident.id})
 
 
 @mod_api.route('/agents/<int:agent_id>', methods=['GET'])
