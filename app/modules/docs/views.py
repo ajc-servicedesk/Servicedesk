99a7c07c814d055f9216ab4a2ae6eda4a355f38e
Ashley Collinge <ashley.collinge@outlook.comp>
2019-05-04 01:18:59 +0100

    trying another one

--- app/modules/docs/views.py
+++ app/modules/docs/views.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask
 from flask import Blueprint, request, current_app
 
-mod_docs = Blueprint('docs', __name__, url_prefix='/docs', static_folder = os.path.join('..\\..\\..\\docs\\_build\\html'))
+mod_docs = Blueprint('docs', __name__, url_prefix='/docs', static_folder = '..\\..\\..\\docs\\_build\\html')
 
 @mod_docs.route('/', defaults={'path': 'index.html'})
 @mod_docs.route('/<path:path>')
