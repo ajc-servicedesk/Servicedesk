ba11ebf5ca781927c5864ad9cf191ddd50386de1
Ashley Collinge <ashley.collinge@outlook.comp>
2019-05-04 01:43:47 +0100

    updated docuemtnation

--- app/modules/api/views.py
+++ app/modules/api/views.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import Blueprint, request
+from flask import Blueprint, request, jsonify
 
 # Define the blueprint: 'auth', set its url prefix: app.url/auth
 mod_api = Blueprint('api', __name__, url_prefix='/api')
@@ -12,3 +12,18 @@ def hello():
     available
 	"""
 	return "Online"
+
+@mod_api.route('/tickets', methods=['GET'])
+def get_tickets(ticket_id: str) -> jsonify:
+	"""
+	Retrieves a list of all tickets and returns the basic information
+	for each ticket. Further API calls would be required to get further
+	information.
+	
+	Args
+		:ticket_id (str): Unique ID for the ticket.
+
+	Returns:
+		Returns json message.
+	"""
+	return jsonify({"test":"test"})
