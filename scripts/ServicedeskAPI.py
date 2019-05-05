c0eff841f60890fcdbe5d59c57dc2d74007e009d
Ashley Collinge <ashley.collinge@outlook.comp>
2019-05-05 02:59:20 +0100

    update cmdline

--- scripts/ServicedeskAPI.py
+++ scripts/ServicedeskAPI.py
@@ -76,5 +76,24 @@ def new_category():
     print(r.status_code)
 
 
+def get_status():
+    url = 'https://apt-servicedesk.azurewebsites.net/api/status/'
+
+    r = requests.get(url)
+
+    # Response, status etc
+    print(r.text)
+    print(r.status_code)
+
+def new_status():
+    url = 'https://apt-servicedesk.azurewebsites.net/api/status/'
+
+    r = requests.post(url)
+
+    # Response, status etc
+    print(r.text)
+    print(r.status_code)
+
+
 if __name__ == '__main__':
     globals()[sys.argv[1]]()
