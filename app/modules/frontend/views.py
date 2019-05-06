a8ace944d36ac01f31756eab99e941eccd2381c2
Ashley Collinge <ashley.collinge@outlook.comp>
2019-05-06 19:01:18 +0100

    grgr

--- app/modules/frontend/views.py
+++ app/modules/frontend/views.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import Blueprint, request, jsonify, render_template
 from app import db
 from app.modules.api.models import *
+from app import db
 
 mod_frontend = Blueprint('frontend', __name__, url_prefix='')
 
@@ -18,3 +19,12 @@ def administration():
 @mod_frontend.route('/incidents', methods=['GET'])
 def incidents():
     return render_template("mod_frontend/incidents.html")
+
+@mod_frontend.route('/incidents/<incident_id>', methods=['GET'])
+def get_more_incident_information(incident_id):
+    """
+    Give more information about the incident and where notes are
+    shown to the user
+    """
+    incident = db.session.query(Incident).filter_by(id=incident_id).one()
+    return render_template("mod_frontend/incident_full.html", incident=incident)
